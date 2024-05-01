0ba2841a60a852945418d71bc1087539894182bd
ailtonmachador <ailton.machadojr@gmail.com>
2024-05-01 08:11:21 +0000

    questionary for experts created, but has to connected with database

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SideComponent } from './side/side.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, SideComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
